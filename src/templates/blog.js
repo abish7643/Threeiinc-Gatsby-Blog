61656e483f83df9f056a87edbc1318798c737aeb
Abish <abishvijayan7643@gmail.com>
2020-01-03 23:47:02 +0530

    Blog Page_Date Posted

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -69,7 +69,7 @@ export const query = graphql`
                 title
                 id
             }
-            createdAt
+            createdAt(formatString: "MMMM DD, YYYY")
             content {
                 childMarkdownRemark {
                     html
