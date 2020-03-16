f11477687ad592c5350d4e6ab82049efcc776d20
Abish <abishvijayan7643@gmail.com>
2020-03-16 23:24:11 +0530

    Featured Page Author Info Addition

--- src/components/featured/index.js
+++ src/components/featured/index.js
@@ -20,6 +20,16 @@ export default () => (
                     id
                     slug
                     title
+                    authorData{
+                        authorName
+                        authorSlug
+                    }
+                    category {
+                        title
+                        id
+                    }
+                    readDuration
+                    createdAt(formatString: "MMMM DD, YYYY")
                     shortDescription
                     featuredImage {
                         fluid(maxWidth: 1200, quality: 85) {
@@ -43,7 +53,10 @@ render={data => (
                     <div className='header__info'>
                         <h2 className='banner___title'>{edge.node.title}</h2>
                         <p className='header__subtitle'>{edge.node.shortDescription}</p>
-                        <button onClick={() => navigate(`/blog/${edge.node.slug}`)} className='btn__med'>
+                        <p className='header__author'>
+                        <a style={{textDecoration:'none', color: 'white'}} onClick={() => navigate(`/idiots/${edge.node.authorData.authorSlug}`)}> {edge.node.authorData.authorName} | {edge.node.createdAt}</a>
+                        </p>
+                        <button style={{marginTop: '5px'}} onClick={() => navigate(`/blog/${edge.node.slug}`)} className='btn__med'>
                             Read More
                         </button>
                     </div>
