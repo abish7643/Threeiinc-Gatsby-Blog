05b3e78970fbbfd4809d82da1c69ea0700e4eda6
Abish <abishvijayan7643@gmail.com>
2020-10-23 15:46:36 +0530

    Aloglia Error

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,6 +35,7 @@ module.exports = {
                     "shortDescription",
                     "featuredImage",
                     "title",
+                    "authorName",
                 ],
             },
         },
